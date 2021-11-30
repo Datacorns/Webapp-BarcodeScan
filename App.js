c98d8c2b39c1b8850f83429f4e0f16ae60651758
VadlaSumith <88647840+VadlaSumith@users.noreply.github.com>
2021-11-30 23:28:13 +0530

    client

--- App.js
+++ App.js
@@ -1,102 +1,111 @@
-
-import React,{useState,useRef} from 'react';
-import {Container , Card, CardContent , makeStyles,Grid,TextField,Button} from '@material-ui/core';
-import QRCode from 'qrcode';
-import QrReader from 'react-qr-reader';
+import React,{useState} from "react";
+import Axios from 'axios';
+import './App.css';
 
 function App() {
-  const [text, setText] = useState('');
-  const [imageUrl ,setImageUrl] =useState('');
-  const [scanResultFile, setScanResultFile] = useState();
-  const [scanResultWebCam, setScanResultWebCam] = useState();
-  const classes = useStyles();
-  const qrRef = useRef();
 
-  const generateQrCode = async () =>{
-    try{
-          const response = await QRCode.toDataURL(text);
-          setImageUrl(response);
-    }catch(error){
-      console.log(error);
+const [firstnameReg,setfirstnameReg] = useState('')
+const [lastnameReg,setlastnameReg] = useState('')
+
+const [usernameReg,setusernameReg] = useState('')
+const [passwordReg,setPasswordReg] = useState('')
+
+const [username,setusername] = useState('')
+const [password,setPassword] = useState('')
+
+const [LoginStatus, setLoginStatus] = useState('');
+
+const register = ()=>{
+  Axios.post('http://localhost:5003/register', {
+    firstname :firstnameReg,
+    lastname :lastnameReg,
+    username: usernameReg,
+    password :passwordReg
+  }).then((response)=>{
+    console.log(response);
+  });
+
+};
+
+const login = ()=>{
+  Axios.post('http://localhost:5003/login', {
+    username: username,
+    password :password,
+  }).then((response)=>{
+
+    if(response.data.message){
+      setLoginStatus(response.data.message)
+     
+    }else{
+      setLoginStatus(response.data[0].username)   
     }
-  }
-  const handleErrorFile = (error) =>{
-    console.log(error);
-  }
-  const handleScanFile = (result) =>{
-      if(result){
-          setScanResultFile(result);
-      }
-  }
-  const onScanFile = () =>{
-    qrRef.current.openImageDialog();
-  }
-  const handleErrorWebCam =(error) =>{
-    console.log(error);
-  }
-  const handleScanWebCam =(result) =>{
-    if(result){
-      setScanResultWebCam(result);
+    console.log(response.data);
+    if(response.data){
+       window.location.href="http://localhost:3000/";
     }
-  }
+    
+    
+  });
+};
+
   return (
-    <Container className = {classes.container}>
-          <Card>
-            <h2 className = {classes.title}>Generate Download & Scan QR Code</h2>
-            <CardContent>
-                <Grid container spacing = {2}> 
-                  <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
-                    <TextField label = "Enter Text Here" onChange={(e) => setText(e.target.value)}/>
-                    <Button className = {classes.btn} variant="contained" 
-                    color="primary" onClick={()=>generateQrCode()}>Generate</Button>
-                    <br/>
-                    <br/>                    
-                    {imageUrl ? (<a href={imageUrl} download> <img src={imageUrl} alt="img"/></a>) : null}
-                  </Grid>
-                  <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
-                    <Button className = {classes.btn} variant="contained" color = "secondary" onClick ={onScanFile}>Scan Qr Code</Button>
-                    <QrReader
-                      ref={qrRef}
-                      delay ={300}
-                      style={{width: '100%'}}
-                      onError={handleErrorFile}
-                      onScan={handleScanFile}
-                      legacyMode
-                    />
-                    <h3>Scanned Code : {scanResultFile}</h3>
-                   </Grid>
-                  <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
-                    <h3>QR Code Scan WebCam</h3>
-                    <QrReader
-                    delay={300}
-                    style={{width : '100%'}}
-                    onError={handleErrorWebCam}
-                    onScan={handleScanWebCam}
-                    />
-                    <h3>Scanned by WebCam :{scanResultWebCam}</h3>
-                  </Grid>
-                </Grid>
-            </CardContent>
-          </Card>
+    <div className="App">
+      <div className ="registration">
+        <h1>Registration</h1>
 
-    </Container>
+        <label>FirstName : </label>
+        <input type="text"
+        onChange={(e)=> {
+          setfirstnameReg(e.target.value);
+        }}
+        /><br/>
+
+        <label>LastName : </label>
+        <input type="text"
+        onChange={(e)=> {
+          setlastnameReg(e.target.value);
+        }}
+        /><br/>
+
+
+        <label>Username : </label>
+        <input type="text"
+          onChange={(e)=> {
+            setusernameReg(e.target.value);
+          }}
+        /><br/>
+        <label>Password : </label>
+        <input type="text"
+        onChange={(e)=> {
+          setPasswordReg(e.target.value);
+        }}
+        /><br/>
+        <button onClick={register}>Register</button>
+      </div>
+
+      <div className ="login">
+        <h1>Login</h1>
+
+        <label>Username : </label>
+        <input type="text" placeholder="Username..."
+         onChange={(e)=> {
+          setusername(e.target.value);
+        }}
+        /><br/>
+
+        <label>Password : </label>
+        <input type="text" placeholder="Password..."
+         onChange={(e)=> {
+          setPassword(e.target.value);
+        }}
+        /><br/>
+
+        <button onClick={login }>Login</button>
+      </div>  
+
+      <h1>{LoginStatus}</h1>
+    </div>
   );
 }
-const useStyles = makeStyles((theme) => ({
-  Container:{
-    marginTop:10
-  },
-  title:{
-    display:'flex',
-    justifyContent:'center',
-    alignItems:'center',
-    background: '#3f51b5',
-    color:'#fff',
-    padding:20
-  },
-  btn:{
-    marginTop:10,
-    marginBottom:20
-  }
-}));
+
 export default App;
--- index.js
+++ index.js
@@ -1,77 +1,23 @@
-const express = require('express');
-const mysql = require('mysql');
-const cors = require("cors");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const bcrypt = require('bcrypt');
-const { response } = require('express');
-const saltRounds = 10;
-
-
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-
-//create connection
-const db = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : 'root',
-    database : 'lowt1'
- });
-
- //create route
- app.post("/register", (req,res) =>{
-    const username = req.body.username
-    const password = req.body.password
-    const firstname = req.body.firstname
-    const lastname = req.body.lastname
-
-    bcrypt.hash(password,saltRounds,(err,hash)=>{
-
-        if(err){
-            console.log(err)
-        }
-        db.query (
-            "INSERT INTO usercredentials(username,password) VALUES (?,?)",
-            [username,hash],
-            (err,result) =>{
-                console.log(err);
-            }
-        );
-    })
-    
-});
-        
-app.post("/login", (req,res) =>{
-    const firstname = req.body.firstname;
-    const username = req.body.username;
-    const password = req.body.password;
-
-    db.query (
-        "SELECT *  FROM usercredentials WHERE username =? ;",
-        username,
-        (err,result) =>{
-            if(err){
-            res.send({err: err});
-            }
-                if (result.length > 0){
-                    bcrypt.compare(password, result[0].password,(error,response)=>{
-                        if(response) {
-                            console.log(result);           
-                            res.send(result)
-                        } else{
-                            res.send({message:"wrong password combo"});
-                        }                 
-                       })
-                } else{
-                    res.send({message:"User doesnt exist"});
-                }
-            }
-    );
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 
- app.listen(5003 , ()=>{
-    console.log('Login Server started 5003 ....');
-});
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+
+
+
+
